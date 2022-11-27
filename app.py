ec5f068ed9707401b2a4b9999c2825754dc99d53
live.comadelkuanysheva7 <adelkuanysheva7@gmail.com>
2022-11-26 23:04:32 -0800

    test3

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def health():
 app = connexion.FlaskApp(__name__, specification_dir='')
 CORS(app.app)
 app.app.config['CORS_HEADERS'] = 'Content-Type'
-adel = "test"
+adel = "test3"
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
